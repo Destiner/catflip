e3de34d71ae02e3c166f4ace7c946c1bae4ee679
Timur Badretdinov <destinerx@gmail.com>
2020-10-04 18:41:18 +0300

    Rename Compound overview page

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router';
 
 import App from './App.vue';
 
-import CompoundMain from './pages/compound/Main.vue';
+import CompoundOverview from './pages/compound/Overview.vue';
 import CompoundProposals from './pages/compound/Proposals.vue';
 import MakerOverview from './pages/maker/Overview.vue';
 import MakerChangelog from './pages/maker/Changelog.vue';
@@ -14,7 +14,7 @@ Vue.use(VueRouter);
 const routes = [
 	{ path: '/', redirect: '/maker/' },
 	{ path: '/changelog', redirect: '/maker/changelog' },
-	{ path: '/compound/', component: CompoundMain },
+	{ path: '/compound/', component: CompoundOverview },
 	{ path: '/compound/proposals', component: CompoundProposals },
 	{ path: '/maker/', component: MakerOverview },
 	{ path: '/maker/changelog', component: MakerChangelog },
