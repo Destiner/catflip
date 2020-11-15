07aff1a57d233a8f0e0d6e8b9c3da3ad37641664
Timur Badretdinov <destinerx@gmail.com>
2020-11-15 13:20:12 +0300

    Add Compound AccountList page to router

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue';
 
 import CompoundOverview from './pages/compound/Overview.vue';
 import CompoundProposals from './pages/compound/Proposals.vue';
+import CompoundAccountList from './pages/compound/AccountList.vue';
 import MakerOverview from './pages/maker/Overview.vue';
 import MakerChangelog from './pages/maker/Changelog.vue';
 import MakerVoting from './pages/maker/Voting.vue';
@@ -14,6 +15,7 @@ const routes = [
 	{ path: '/changelog', redirect: '/maker/changelog' },
 	{ path: '/compound/', component: CompoundOverview },
 	{ path: '/compound/proposals', component: CompoundProposals },
+	{ path: '/compound/accounts', component: CompoundAccountList },
 	{ path: '/maker/', component: MakerOverview },
 	{ path: '/maker/changelog', component: MakerChangelog },
 	{ path: '/maker/voting', component: MakerVoting },
